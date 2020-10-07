b2759a7268598f3d7a66afe8d5cc84969559f376
mgmtadmin <brandon.sorgdrager@gmail.com>
2020-10-06 21:32:56 -0500

    Remove sonny

--- resources.tf
+++ resources.tf
@@ -623,13 +623,3 @@ resource "kubernetes_namespace" "sonny" {
   }
 }
 
-resource "helm_release" "sonny" {
-  repository = "https://bsord.github.io/helm-charts"
-  chart      = "sonny"
-  name       = "sonny"
-  namespace  = "sonny"
-  set_sensitive {
-    name  = "HUBOT_SLACK_TOKEN"
-    value = var.hubot_slack_token_sonny
-  }
-}
