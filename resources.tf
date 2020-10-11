f20672cc6ab464efa4a0552c541687f6ee376526
Jon Fairbanks <fairplay89@gmail.com>
2020-10-11 00:50:52 -0700

    Remove F5

--- resources.tf
+++ resources.tf
@@ -213,65 +213,6 @@ resource "cloudflare_record" "tetris" {
   ttl     = 1
 }
 
-## F5
-
-resource "kubernetes_namespace" "f5oclock" {
-  metadata {
-    name = "f5oclock"
-  }
-}
-
-resource "helm_release" "f5-api" {
-  repository = "https://jonfairbanks.github.io/helm-charts"
-  chart      = "f5-api"
-  name       = "f5-api"
-  namespace  = "f5oclock"
-  set_sensitive {
-    name  = "MONGO_URI"
-    value = var.f5_mongo_uri
-  }
-  set {
-    name  = "image.tag"
-    value = "1.0.0"
-  }
-}
-
-resource "helm_release" "f5-web" {
-  repository = "https://jonfairbanks.github.io/helm-charts"
-  chart      = "f5-web"
-  name       = "f5-web"
-  namespace  = "f5oclock"
-  set {
-    name  = "ingress.enabled"
-    value = "true"
-  }
-  set {
-    name  = "ingress.hosts[0].host"
-    value = cloudflare_record.f5-web.hostname
-  }
-  set {
-    name  = "ingress.hosts[0].paths[0]"
-    value = "/"
-  }
-  set_sensitive {
-    name  = "MONGO_URI"
-    value = var.f5_mongo_uri
-  }
-  set {
-    name  = "image.tag"
-    value = "1.0.1"
-  }
-}
-
-resource "cloudflare_record" "f5-web" {
-  zone_id = var.cloudflare_zone_id_fairbanks
-  name    = "f5"
-  proxied = true
-  value   = data.kubernetes_service.nginx-ingress-controller.load_balancer_ingress.0.ip
-  type    = "A"
-  ttl     = 1
-}
-
 ## Halbert
 
 resource "kubernetes_namespace" "halbert" {
