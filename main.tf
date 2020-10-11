5008b042be43a391a92966432ec3ec245e7d4dce
Jon Fairbanks <fairplay89@gmail.com>
2020-10-11 00:57:22 -0700

    Fmt

--- main.tf
+++ main.tf
@@ -15,22 +15,28 @@ locals {
 }
 
 module "docker-node-app" {
-  source             = "./apps/docker-node-app"
-  providers          = "${local.providers}"
+  source = "./apps/docker-node-app"
+  providers = {
+    cloudflare = cloudflare
+  }
   cloudflare_zone_id = "${var.cloudflare_zone_id_fairbanks}"
   do_cluster_name    = "${var.do_cluster_name}"
 }
 
 /* module "docker-node-app-bsord" {
   source    = "./apps/docker-node-app"
-  providers = "${local.providers}"
+  providers = {
+    cloudflare = cloudflare
+  }
   cloudflare_zone_id = "${var.cloudflare_zone_id}"
   do_cluster_name    = "${var.do_cluster_name}"
 } */
 
 module "f5oclock" {
-  source             = "./apps/f5oclock"
-  providers          = "${local.providers}"
+  source = "./apps/f5oclock"
+  providers = {
+    cloudflare = cloudflare
+  }
   cloudflare_zone_id = "${var.cloudflare_zone_id_fairbanks}"
   do_cluster_name    = "${var.do_cluster_name}"
   f5_mongo_uri       = "${var.f5_mongo_uri}"
