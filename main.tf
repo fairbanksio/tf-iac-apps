259cdbd8da63fda28d91a585179fed2ad32bfff4
Jon Fairbanks <fairplay89@gmail.com>
2020-10-10 23:42:53 -0700

    asdf

--- main.tf
+++ main.tf
@@ -8,12 +8,20 @@ terraform {
   }
 }
 
+locals {
+  default = {
+    cloudflare_zone_id                          = "${var.cloudflare_zone_id}"
+    cloudflare_zone_id_fairbanks                = "${var.cloudflare_zone_id_fairbanks}"
+    kubernetes_service.nginx-ingress-controller = "${data.data.kubernetes_service.nginx-ingress-controller}"
+  }
+}
+
 module "docker-node-app" {
   source = "./apps/docker-node-app"
   providers = {
-    kubernetes = kubernetes
     cloudflare = cloudflare
   }
   cloudflare_zone_id           = "${var.cloudflare_zone_id}"
   cloudflare_zone_id_fairbanks = "${var.cloudflare_zone_id_fairbanks}"
+  kubernetes_service.nginx-ingress-controller = "${data.data.kubernetes_service.nginx-ingress-controller}"
 }
