6ed2bc3849045e37126f22e907bf077237fd5ede
Jon Fairbanks <fairplay89@gmail.com>
2020-10-11 02:25:09 -0700

    Hmmm

--- variables.tf
+++ variables.tf
@@ -4,7 +4,6 @@ variable "cloudflare_email" {}
 variable "cloudflare_api_key" {}
 variable "cloudflare_zone_id" {}
 variable "cloudflare_zone_id_fairbanks" {}
-variable "f5_mongo_uri" {}
 variable "hubot_slack_token" {}
 variable "hubot_slack_token_sonny" {}
 variable "tiles-api_mongouri" {}
