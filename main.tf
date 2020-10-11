c27f9daaaef6c9a18c0b1c1f3d204a1978d82ef0
Jon Fairbanks <fairplay89@gmail.com>
2020-10-11 00:45:36 -0700

    Locals

--- main.tf
+++ main.tf
@@ -8,21 +8,22 @@ terraform {
   }
 }
 
-module "kube-fairbanks-dev" {
-  source = "./apps/docker-node-app"
+locals {
   providers = {
     cloudflare = cloudflare
   }
+}
+
+module "kube-fairbanks-dev" {
+  source = "./apps/docker-node-app"
+  providers = "${local.providers}"
   cloudflare_zone_id = "${var.cloudflare_zone_id_fairbanks}"
   do_cluster_name    = "${var.do_cluster_name}"
 }
 
 /* module "kube-bsord-dev" {
   source    = "./apps/docker-node-app"
-  namespace = "docker-node-app-bsord"
-  providers = {
-    cloudflare = cloudflare
-  }
+  providers = "${local.providers}"
   cloudflare_zone_id = "${var.cloudflare_zone_id}"
   do_cluster_name    = "${var.do_cluster_name}"
 } */
