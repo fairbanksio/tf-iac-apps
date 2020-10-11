a397b130bbcfdbd823e5d6a830d14580369b4b2a
Jon Fairbanks <fairplay89@gmail.com>
2020-10-10 23:22:24 -0700

    Formatting

--- main.tf
+++ main.tf
@@ -10,13 +10,13 @@ terraform {
 
 locals {
   default = {
-    cloudflare_zone_id = var.cloudflare_zone_id
+    cloudflare_zone_id           = var.cloudflare_zone_id
     cloudflare_zone_id_fairbanks = var.cloudflare_zone_id_fairbanks
   }
 }
 
 module "docker-node-app" {
-  source = "./apps/docker-node-app"
+  source  = "./apps/docker-node-app"
   context = local.default
   providers = {
     cloudflare = "cloudflare"
