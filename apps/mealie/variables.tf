d239e5976a82ca48b8d3d1dadb93749445050320
Jon Fairbanks <fairplay89@gmail.com>
2021-06-25 15:38:24 -0700

    Clean up old apps

--- apps/mealie/variables.tf
+++
@@ -1,2 +0,0 @@
-variable "cloudflare_zone_id" {}
-variable "do_cluster_name" {}
--- apps/statuspage-bsord/main.tf
+++
@@ -1,8 +0,0 @@
-resource "cloudflare_record" "status-fairbanks" {
-  zone_id = var.cloudflare_zone_id
-  name    = "status"
-  proxied = false
-  value   = "stats.uptimerobot.com"
-  type    = "CNAME"
-  ttl     = 1
-}
